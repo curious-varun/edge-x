5c34562526ae893c1bc670e2a36054d8d2187773
Varun <devvarunbhardwaj@gmail.com>
2025-04-19 16:18:07 +0530

    ::

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { ThemeToggleButton } from "@/components/global/theme-toggle-button";
-import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/auth";
-import Link from "next/link";
-import Image from "next/image";
-import { Session } from "next-auth";
-
-export default async function Page() {
-  const session = (await auth()) as Session;
-
+export default function Page() {
   return (
     <>
       home page
